fb18323ab8625dd28eac4c6bccfd4c0f36061fa2
Thumul <thumul.dasun@gmail.com>
2025-06-03 09:29:45 +0530

    Refactor the Parser according to grammar rules and Add comments

--- parser.py
+++ parser.py
@@ -22,12 +22,6 @@ class Parser:
             self.position += 1
             return token
         return None
-
-    
-    def prevToken(self):
-        if self.position > 0:
-            return self.tokens[self.position - 1]
-        return None
     
     def reversePos(self):
         if self.position > 0:
@@ -41,7 +35,13 @@ class Parser:
             raise SyntaxError(f"Expected {expected} but found {self.peek()}")
         return token
 
+# Expressions ###################################################
     def parse_E(self):
+        """
+        E   -> 'let' D 'in' E => 'let'
+            -> 'fn' Vb+ '.' E => 'lambda'
+            -> Ew;
+        """
         token = self.peek()
 
         if token and token.type == 'let':
@@ -75,8 +75,11 @@ class Parser:
         else:
             return self.parse_Ew()
 
-
     def parse_Ew(self):
+        """
+        Ew  -> T 'where' Dr => 'where'
+            -> T;
+        """
         t = self.parse_T()
 
         token = self.peek()
@@ -87,40 +90,13 @@ class Parser:
 
         return t
 
-    
-    def parse_Rn(self):
-        token =self.peek()
-        if token:
-            if token.type == 'identifier':
-                self.match('identifier')
-                return RnNode('identifier', token.value)
-            elif token.type == 'integer':
-                self.match('integer')
-                return RnNode('integer', token.value) #can pass int
-            elif token.type == 'string':
-                self.match('string')
-                return RnNode('string', token.value)
-            elif token.type == 'true':
-                self.match('true')
-                return RnNode('true', token.value) #can pass True
-            elif token.type == 'false':
-                self.match('false')
-                return RnNode('false', token.value) #can pass False 
-            elif token.type == 'nil':
-                self.match('nil')
-                return RnNode('nil', token.value) #can pass None
-            elif (token.type == 'delimiter' and token.value == '('):
-                self.match('delimiter')
-                e = self.parse_E()
-                self.expect('delimiter')
-                return e
-            else:
-                raise SyntaxError(f"Unexpected token: {token}")
-        else:
-            raise SyntaxError("Unexpected end of input while parsing RnNode")
 
-
+# Tuple Expressions #############################################
     def parse_T(self):
+        """
+        T   -> Ta(',' Ta )+ => 'tau'
+            -> Ta ;
+        """
         ta_list = [self.parse_Ta()]
 
         while True:
@@ -139,6 +115,10 @@ class Parser:
             return TauNode(ta_list)
 
     def parse_Ta(self):
+        """
+        Ta  -> Ta 'aug' Tc => 'aug'
+            -> Tc ;
+        """
         node = self.parse_Tc()
 
         while True:
@@ -152,52 +132,11 @@ class Parser:
 
         return node
 
-        if not operands:
-            raise SyntaxError("Expected at least one operand")
-        elif len(operands) == 1:
-            return operands[0]
-        else:
-            for i in range(len(operands)-1, -1, -1):
-                Rn = operands[i]
-                # Create a GammaNode for each operand
-                node = GammaNode(node, Rn)
-            return node
-
-    def parse_R(self):
-        operands = []
-        while True:
-            try:
-                Rn = self.parse_Rn()
-                operands.append(Rn)
-            except SyntaxError:
-                # If we can't parse another Rn, break the loop
-                break
-        if not operands:
-            raise SyntaxError("Expected at least one operand")
-        elif len(operands) == 1:
-            return operands[0]
-        else:
-            for i in range(len(operands)-1, -1, -1):
-                Rn = operands[i]
-                # Create a GammaNode for each operand
-                node = GammaNode(node, Rn)
-            return node
-
-    def parse_V1(self):
-        params = []
-        params.append(self.match('identifier')) 
-        while self.peek() and self.peek().type == 'delimiter' and self.peek().value == ',':
-            self.expect('delimiter')
-            params.append(self.match('identifier'))
-        
-        if len(params)==0:
-            raise SyntaxError("Expected at least one identifier in paranthesis")
-        elif len(params) == 1:
-            return VbNode(params[0].value)
-        else:
-            return CommaNode(params)
-
     def parse_Tc(self):
+        """
+        Tc  -> B'->' Tc '|' Tc => '->'
+            -> B ;
+        """
         b = self.parse_B()
         token = self.peek()
         if token and token.value == '->':
@@ -209,7 +148,13 @@ class Parser:
 
         return b
 
+
+# Boolean Expressions ###########################################
     def parse_B(self):
+        """
+        B   -> B'or' Bt => 'or'
+            -> Bt ;
+        """
         node = self.parse_Bt()
         while True :
             if self.peek() and self.peek().value == 'or':
@@ -223,6 +168,10 @@ class Parser:
         return node
 
     def parse_Bt(self):
+        """
+        Bt  -> Bt '&' Bs => '&'
+            -> Bs ;
+        """
         node = self.parse_Bs()
         while True:
             if self.peek() and self.peek().value == '&':
@@ -232,92 +181,12 @@ class Parser:
             else:
                 break
         return node
-            
-    
-    def parse_Db(self):
-        token = self.peek()
-        if token:
-            if token.type == 'identifier':
-                identifier = self.match('identifier')
-                next_token = self.peek()
-                if next_token:
-                    if(next_token.type == 'delimeter' and next_token.value == ','):
-                        self.reversePos()
-                        v1 = self.parse_V1()
-                        self.expect('operator')
-                        e = self.parse_E()
-                        return AssignmentNode(v1, e)
-                    else:
-                        Vbs = []
-                        while True:
-                            vb = self.parse_Vb()
-                            Vbs.append(vb)
-                            if self.peek() and self.peek().value == '=':
-                                break
-                        if len(Vbs) == 0:
-                            raise SyntaxError("Expected at least one variable binding")
-                        self.expect('=')
-                        e = self.parse_E()
-                        return FcnFormNode(identifier.value, Vbs, e)
-                else:
-                    raise SyntaxError("Unexpected end of input while parsing DbNode")
-            elif token.type == 'delimiter' and token.value == '(':
-                self.match('(')
-                d = self.parse_D()
-                self.expect(')')
-                return d
-            else:
-                raise SyntaxError(f"Unexpected token: {token}")
-        else:
-            raise SyntaxError("Unexpected end of input while parsing DbNode")
-        
-    def parse_Dr(self):
-        token = self.peek()
-        if token:
-            if token.type == 'rec':
-                self.expect('rec')
-                Db = self.parse_Db()
-                return RecNode(Db)
-            else:
-                return self.parse_Db()
-        else:
-            raise SyntaxError("Unexpected end of input while parsing DrNode")
-        
-    def parse_Da(self):
-        token = self.peek()
-        if token:
-            Drs = []
-            while True:
-                Dr = self.parse_Dr()
-                Drs.append(Dr)
-                if self.peek() and self.peek().value == 'and':
-                    self.expect('and')
-                else:
-                    break
-            if len(Drs) == 0:
-                raise SyntaxError("Expected at least one declaration")
-            elif len(Drs) == 1:
-                return Drs[0]
-            else:
-                return AndNode(Drs)
-        else:
-            raise SyntaxError("Unexpected end of input while parsing DaNode")
-        
-    def parse_D(self):
-        token = self.peek()
-        if token:
-            Da = self.parse_Da()
-            if self.peek() and self.peek().value == 'within':
-                self.expect('within')
-                D = self.parse_D()
-                return WithinNode(Da, D)
-            else:
-                return Da
-        else:
-            raise SyntaxError("Unexpected end of input while parsing DNode")
-
 
     def parse_Bs(self):
+        """
+        Bs  -> 'not' Bp => 'not'
+            -> Bp ;
+        """
         token = self.peek()
         if token and token.value == 'not':
             self.match('not')
@@ -327,6 +196,15 @@ class Parser:
         return self.parse_Bp()
 
     def parse_Bp(self):
+        """
+        Bp  -> A ('gr' | '>' ) A => 'gr'
+            -> A ('ge' | '>=') A => 'ge'
+            -> A ('ls' | '<' ) A => 'ls'
+            -> A ('le' | '<=') A => 'le'
+            -> A 'eq' A          => 'eq'
+            -> A 'ne' A          => 'ne'
+            -> A ;
+        """
         a1 = self.parse_A()
         token = self.peek()
 
@@ -362,7 +240,16 @@ class Parser:
 
         return a1
 
+
+# Arithmetic Expressions ########################################
     def parse_A(self):
+        """
+        A   -> A '+' At => '+'
+            -> A '-' At => '-'
+            -> '+' At
+            -> '-' At   => 'neg'
+            -> At ;
+        """
         token = self.peek()
 
         if token and token.value == '+':
@@ -391,6 +278,11 @@ class Parser:
         return node
 
     def parse_At(self):
+        """
+        At  -> At '*' Af => '*'
+            -> At '/' Af => '/'
+            -> Af ;
+        """
         node = self.parse_Af()
         while True:
             token = self.peek()
@@ -407,6 +299,10 @@ class Parser:
         return node
 
     def parse_Af(self):
+        """
+        Af  -> Ap '**' Af => '**'
+            -> Ap ;
+        """
         node = self.parse_Ap()
         while True:
             token = self.peek()
@@ -419,6 +315,10 @@ class Parser:
         return node
 
     def parse_Ap(self):
+        """
+        Ap  -> Ap '@' '<IDENTIFIER>' R => '@'
+            -> R ;
+        """
         node = self.parse_R()
 
         while True:
@@ -438,3 +338,210 @@ class Parser:
                 break
 
         return node
+
+
+# Rators and Rands ##############################################
+    def parse_R(self):
+        """
+        R   -> R Rn => 'gamma'
+            -> Rn ;
+        """
+        operands = []
+        while True:
+            try:
+                Rn = self.parse_Rn()
+                operands.append(Rn)
+            except SyntaxError:
+                # If we can't parse another Rn, break the loop
+                break
+        if not operands:
+            raise SyntaxError("Expected at least one operand")
+        elif len(operands) == 1:
+            return operands[0]
+        else:
+            for i in range(len(operands)-1, -1, -1):
+                Rn = operands[i]
+                # Create a GammaNode for each operand
+                node = GammaNode(node, Rn)
+            return node
+        
+    def parse_Rn(self):
+        """
+        Rn  -> '<IDENTIFIER>'
+            -> '<INTEGER>'
+            -> '<STRING>'
+            -> 'true'   => 'true'
+            -> 'false'  => 'false'
+            -> 'nil'    => 'nil'
+            -> '(' E ')'
+            -> 'dummy'  => 'dummy' ; - hasn't implemented
+        """
+        token =self.peek()
+        if token:
+            if token.type == 'identifier':
+                self.match('identifier')
+                return RnNode('identifier', token.value)
+            elif token.type == 'integer':
+                self.match('integer')
+                return RnNode('integer', token.value) #can pass int
+            elif token.type == 'string':
+                self.match('string')
+                return RnNode('string', token.value)
+            elif token.type == 'true':
+                self.match('true')
+                return RnNode('true', token.value) #can pass True
+            elif token.type == 'false':
+                self.match('false')
+                return RnNode('false', token.value) #can pass False 
+            elif token.type == 'nil':
+                self.match('nil')
+                return RnNode('nil', token.value) #can pass None
+            elif (token.type == 'delimiter' and token.value == '('):
+                self.match('delimiter')
+                e = self.parse_E()
+                self.expect('delimiter')
+                return e
+            else:
+                raise SyntaxError(f"Unexpected token: {token}")
+        else:
+            raise SyntaxError("Unexpected end of input while parsing RnNode")
+
+
+# Definitions ###################################################
+    def parse_D(self):
+        """
+        D   -> Da 'within' D => 'within'
+            -> Da ;
+        """
+        token = self.peek()
+        if token:
+            Da = self.parse_Da()
+            if self.peek() and self.peek().value == 'within':
+                self.expect('within')
+                D = self.parse_D()
+                return WithinNode(Da, D)
+            else:
+                return Da
+        else:
+            raise SyntaxError("Unexpected end of input while parsing DNode")
+
+    def parse_Da(self):
+        """
+        Da  -> Dr ( 'and' Dr )+ => 'and'
+            -> Dr ;
+        """
+        token = self.peek()
+        if token:
+            Drs = []
+            while True:
+                Dr = self.parse_Dr()
+                Drs.append(Dr)
+                if self.peek() and self.peek().value == 'and':
+                    self.expect('and')
+                else:
+                    break
+            if len(Drs) == 0:
+                raise SyntaxError("Expected at least one declaration")
+            elif len(Drs) == 1:
+                return Drs[0]
+            else:
+                return AndNode(Drs)
+        else:
+            raise SyntaxError("Unexpected end of input while parsing DaNode")
+
+    def parse_Dr(self):
+        """
+        Dr  -> 'rec' Db => 'rec'
+            -> Db ;
+        """
+        token = self.peek()
+        if token:
+            if token.type == 'rec':
+                self.expect('rec')
+                Db = self.parse_Db()
+                return RecNode(Db)
+            else:
+                return self.parse_Db()
+        else:
+            raise SyntaxError("Unexpected end of input while parsing DrNode")
+
+    def parse_Db(self):
+        """
+        Db  -> Vl '=' E                 => '='
+            -> '<IDENTIFIER>' Vb+ '=' E => 'fcn_form'
+            -> '(' D ')' ;
+        """
+        token = self.peek()
+        if token:
+            if token.type == 'identifier':
+                identifier = self.match('identifier')
+                next_token = self.peek()
+                if next_token:
+                    if(next_token.type == 'delimeter' and next_token.value == ','):
+                        self.reversePos()
+                        v1 = self.parse_V1()
+                        self.expect('operator')
+                        e = self.parse_E()
+                        return AssignmentNode(v1, e)
+                    else:
+                        Vbs = []
+                        while True:
+                            vb = self.parse_Vb()
+                            Vbs.append(vb)
+                            if self.peek() and self.peek().value == '=':
+                                break
+                        if len(Vbs) == 0:
+                            raise SyntaxError("Expected at least one variable binding")
+                        self.expect('=')
+                        e = self.parse_E()
+                        return FcnFormNode(identifier.value, Vbs, e)
+                else:
+                    raise SyntaxError("Unexpected end of input while parsing DbNode")
+            elif token.type == 'delimiter' and token.value == '(':
+                self.match('(')
+                d = self.parse_D()
+                self.expect(')')
+                return d
+            else:
+                raise SyntaxError(f"Unexpected token: {token}")
+        else:
+            raise SyntaxError("Unexpected end of input while parsing DbNode")
+
+
+# Variables #####################################################
+    def parse_Vb(self):
+        """
+        Vb  -> '<IDENTIFIER>'
+            -> '(' Vl ')'
+            -> '(' ')'         => '()' - hasn't implemented
+        """
+        token = self.peek()
+        if token:
+            if token.type == 'identifier':
+                identifier = self.match('identifier')
+                return VbNode(identifier.value)
+            elif token.type == 'delimiter' and token.value == '(':
+                self.match('delimiter')
+                vb_list = self.parse_V1()
+                self.expect('delimiter')
+                return vb_list
+            else:
+                raise SyntaxError(f"Unexpected token: {token}")
+
+    def parse_V1(self):
+        """
+        Vl  -> '<IDENTIFIER>' list ','  => ','
+        """
+        params = []
+        params.append(self.match('identifier')) 
+        while self.peek() and self.peek().type == 'delimiter' and self.peek().value == ',':
+            self.expect('delimiter')
+            params.append(self.match('identifier'))
+        
+        if len(params)==0:
+            raise SyntaxError("Expected at least one identifier in paranthesis")
+        elif len(params) == 1:
+            return VbNode(params[0].value)
+        else:
+            return CommaNode(params)
+
