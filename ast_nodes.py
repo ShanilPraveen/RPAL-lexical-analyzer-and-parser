8fa98581c6fda0bedb61cb351f54881dfa419d36
Thumul <thumul.dasun@gmail.com>
2025-06-03 13:27:43 +0530

    Add standardize feature for the all Node classes

--- ast_nodes.py
+++ ast_nodes.py
@@ -1,12 +1,6 @@
 from abc import ABC, abstractmethod
 
-__all__ = [
-    'ASTNode', 
-    'LambdaNode', 
-    'LetNode',
-    ]
-
-class ASTNode(ABC):
+class Node(ABC):
     indentationSymbol = '.'
 
     def __init__(self, type_, value):
@@ -29,16 +23,37 @@ class ASTNode(ABC):
 
     def __repr__(self):
         return self.__str__()
-
     
-class LambdaNode(ASTNode):
+class STLambdaNode(Node):
+    def __init__(self, Vb, Exp):
+        super().__init__('E', 'lambda')
+        self.Vb = Vb
+        self.E = Exp
+    
+    def standardize(self):
+        return self
+    
+    def print(self, indent=0):
+        print(f'{self.indentationSymbol * indent}{self.value}')
+        self.Vb.print(indent + 1)
+        self.E.print(indent + 1)
+
+
+class LambdaNode(Node):
     def __init__(self, Vbs, Exp):
         super().__init__('E', 'lambda')
         self.Vb_list = Vbs
         self.E = Exp
     
     def standardize(self):
-        # Standardize logic goes here
+        stVbs = [vb.standardize() for vb in self.Vb_list]
+        stE = self.E.standardize()
+        for i in range(len(stVbs)-1, -1, -1):
+            node = STLambdaNode(stVbs[i], stE)
+            stE = node
+        return stE  # Return the standardized lambda node
+    
+    def __str__(self):
         return f"Lambda({self.Vb_list}, {self.E})"
     
     def print(self, indent=0):
@@ -46,15 +61,20 @@ class LambdaNode(ASTNode):
         for vb in self.Vb_list:
             vb.print(indent + 1)
         self.E.print(indent + 1)
-    
-class GammaNode(ASTNode):
+
+
+class GammaNode(Node):
     def __init__(self, N, E):
         super().__init__('gamma', 'gamma')
         self.N = N
         self.E = E
     
     def standardize(self):
-        # Standardize logic goes here
+        stN = self.N.standardize()
+        stE = self.E.standardize()
+        return GammaNode(stN, stE) 
+    
+    def __str__(self):
         return f"Gamma({self.N}, {self.E})"
 
     def print(self, indent=0):
@@ -63,12 +83,14 @@ class GammaNode(ASTNode):
         self.E.print(indent + 1)    
 
 
-class RnNode(ASTNode):
+class RnNode(Node):
     def __init__(self, randType, rand):
         super().__init__(randType, rand)
     
     def standardize(self):
-        # Standardize logic goes here
+        return self # Return the node itself, as Rn is already standardized
+    
+    def __str__(self):
         return f"Rn({self.type}, {self.value})"
     
     def print(self, indent=0):
@@ -85,62 +107,83 @@ class RnNode(ASTNode):
         print(f'{self.indentationSymbol * indent}{tag}')
 
     
-class LetNode(ASTNode):
+class LetNode(Node):
     def __init__(self, Def, Exp):
         super().__init__('E', 'let')
         self.D = Def
         self.E = Exp
     
     def standardize(self):
-        # Standardize logic goes here
+        stD = self.D.standardize()
+        stE = self.E.standardize()
+        if isinstance(stD, AssignmentNode):
+            lambdaNode = STLambdaNode(stD.v1, stE)
+            return GammaNode(lambdaNode, stD.e)
+        else:
+            raise ValueError("Invalid definition in LetNode")
+        
+    def __str__(self):
         return f"Let({self.D}, {self.E})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         self.D.print(indent + 1)
         self.E.print(indent + 1)
-    
-class CommaNode(ASTNode):
+
+
+class CommaNode(Node):
     def __init__(self, params):
         super().__init__('comma', ',')
         self.params = params
     
     def standardize(self):
-        # Standardize logic goes here
+        stParams = [param.standardize() for param in self.params]
+        return CommaNode(stParams)  # Return a new CommaNode with standardized parameters
+    
+    def __str__(self):
         return f"Comma({self.params})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         for param in self.params:
             param.print(indent + 1)
-    
-class VbNode(ASTNode):
+
+
+class VbNode(Node):
     def __init__(self, name):
         super().__init__('Vb', name)
     
     def standardize(self):
-        # Standardize logic goes here
+        return self  # Return the node itself, as Vb is already standardized
+    
+    def __str__(self):
         return f"Vb({self.value})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}<ID:{self.value}>')
-    
-class AssignmentNode(ASTNode):
+
+
+class AssignmentNode(Node):
     def __init__(self, v1, Exp):
         super().__init__('assignment', '=')
         self.v1 = v1
         self.e = Exp
     
     def standardize(self):
-        # Standardize logic goes here
+        stV1 = self.v1.standardize()
+        stE = self.e.standardize()
+        return AssignmentNode(stV1, stE)  # Return a new AssignmentNode with standardized components
+    
+    def __str__(self):
         return f"Assignment({self.v1}, {self.e})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         self.v1.print(indent + 1)
         self.e.print(indent + 1)
-    
-class FcnFormNode(ASTNode):
+
+
+class FcnFormNode(Node):
     def __init__(self, name, Vbs, Exp):
         super().__init__('fcn_form', 'function')
         self.name = name
@@ -148,7 +191,14 @@ class FcnFormNode(ASTNode):
         self.E = Exp
     
     def standardize(self):
-        # Standardize logic goes here
+        stVbs = [vb.standardize() for vb in self.Vbs]
+        stE = self.E.standardize()
+        for i in range(len(stVbs)-1, -1, -1):
+            node = STLambdaNode(stVbs[i], stE)
+            stE = node
+        return AssignmentNode(self.name, stE)  # Return an AssignmentNode with the function name and standardized expression
+    
+    def __str__(self):
         return f"FcnForm({self.Vbs}, {self.E})"
     
     def print(self, indent=0):
@@ -157,27 +207,51 @@ class FcnFormNode(ASTNode):
         for vb in self.Vbs:
             vb.print(indent + 1)
         self.E.print(indent + 1)
-    
-class RecNode(ASTNode):
+
+
+class RecNode(Node):
     def __init__(self, Db):
         super().__init__('rec', 'rec')
         self.Db = Db
     
     def standardize(self):
-        # Standardize logic goes here
+        stDb = self.Db.standardize()
+        if isinstance(stDb, AssignmentNode):
+            lambdaNode = STLambdaNode(stDb.v1, stDb.e)
+            gammaNode = GammaNode('Y', lambdaNode)
+            return AssignmentNode(stDb.v1, gammaNode)  # Return an AssignmentNode for the recursive definition
+        else:
+            raise ValueError("Invalid definition in RecNode")
+    
+    def __str__(self):
         return f"Rec({self.Db})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         self.Db.print(indent + 1)
-    
-class AndNode(ASTNode):
+
+
+class AndNode(Node):
     def __init__(self, Drs):
         super().__init__('and', 'and')
         self.Drs = Drs
 
     def standardize(self):
-        # Standardize logic goes here
+        stDrs = [dr.standardize() for dr in self.Drs]
+        params = []
+        elements = []
+
+        for dr in stDrs:
+            if not isinstance(dr, AssignmentNode):
+                raise ValueError("Invalid Node in AndNode. Expected an AssignmentNode.")
+            params.append(dr.v1)
+            elements.append(dr.e)
+        
+        comma = CommaNode(params)
+        tuple = TauNode(elements)
+        return AssignmentNode(comma, tuple)  # Return an AssignmentNode with a CommaNode and TauNode
+    
+    def __str__(self):
         return f"And({self.Drs})"
     
     def print(self, indent=0):
@@ -185,66 +259,95 @@ class AndNode(ASTNode):
         for dr in self.Drs:
             dr.print(indent + 1)
 
-class WithinNode(ASTNode):
+
+class WithinNode(Node):
     def __init__(self, Da, D):
         super().__init__('within', 'within')
         self.Da = Da
         self.D = D
 
     def standardize(self):
-        # Standardize logic goes here
+        stDa = self.Da.standardize()
+        stD = self.D.standardize()
+        if not isinstance(stDa, AssignmentNode) or not isinstance(stD, AssignmentNode):
+            raise ValueError("Invalid Node in WithinNode. Expected AssignmentNodes.")
+        
+        lambdaNode = STLambdaNode(stDa.v1, stD.e)
+        gammaNode = GammaNode(lambdaNode, stDa.e)
+        return AssignmentNode(stD.v1, gammaNode)  # Return an AssignmentNode for the within definition
+
+    def __str__(self):
         return f"Within({self.Da}, {self.D})"
 
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         self.Da.print(indent + 1)
         self.D.print(indent + 1)   
-    
-class WhereNode(ASTNode):
+
+
+class WhereNode(Node):
     def __init__(self, T, Dr):
         super().__init__('Ew', 'where')
         self.T = T
         self.Dr = Dr
     
     def standardize(self):
-        # Standardize logic goes here
+        stT = self.T.standardize()
+        stDr = self.Dr.standardize()
+        if not isinstance(stDr, AssignmentNode):
+            raise ValueError("Invalid Node in WhereNode. Expected an AssignmentNode.")
+        
+        lambdaNode = STLambdaNode(stDr.v1, stT)
+        return GammaNode(lambdaNode, stDr.e)  # Return a GammaNode with the standardized lambda and expression
+    
+    def __str__(self):
         return f"Where({self.T}, {self.Dr})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         self.T.print(indent + 1)
         self.Dr.print(indent + 1)
-    
-class TauNode(ASTNode):
+
+
+class TauNode(Node):
     def __init__(self, elements):
         super().__init__('tau', 'tau')
         self.elements = elements
 
     def standardize(self):
-        # Standardize logic goes here
+        stElements = [element.standardize() for element in self.elements]
+        return TauNode(stElements)  # Return a new TauNode with standardized elements
+    
+    def __str__(self):
         return f"Tau({self.elements})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         for element in self.elements:
             element.print(indent + 1)
-    
-class AugNode(ASTNode):
+
+
+class AugNode(Node):
     def __init__(self, Ta, Tc):
         super().__init__('aug', 'aug')
         self.Ta = Ta
         self.Tc = Tc
     
     def standardize(self):
-        # Standardize logic goes here
+        stTa = self.Ta.standardize()
+        stTc = self.Tc.standardize()
+        return AugNode(stTa, stTc)  # Return a new AugNode with standardized Ta and Tc
+
+    def __str__(self):
         return f"Aug({self.Ta}, {self.Tc})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         self.Ta.print(indent + 1)
         self.Tc.print(indent + 1)
-    
-class ArrowNode(ASTNode):
+
+
+class ArrowNode(Node):
     def __init__(self, condition, ifCase, elseCase):
         super().__init__('arrow', '->')
         self.condition = condition
@@ -252,7 +355,12 @@ class ArrowNode(ASTNode):
         self.elseCase = elseCase
 
     def standardize(self):
-        # Standardize logic goes here
+        stCondition = self.condition.standardize()
+        stIfCase = self.ifCase.standardize()
+        stElseCase = self.elseCase.standardize()
+        return ArrowNode(stCondition, stIfCase, stElseCase)  # Return a new ArrowNode with standardized components
+    
+    def __str__(self):
         return f"Arrow({self.condition}, {self.ifCase}, {self.elseCase})"
     
     def print(self, indent=0):
@@ -260,79 +368,103 @@ class ArrowNode(ASTNode):
         self.condition.print(indent + 1)
         self.ifCase.print(indent + 1)
         self.elseCase.print(indent + 1)
-    
-class NotNode(ASTNode):
+
+
+class NotNode(Node):
     def __init__(self, Bp):
         super().__init__('not', 'not')
         self.Bp = Bp
     
     def standardize(self):
-        # Standardize logic goes here
+        stBp = self.Bp.standardize()
+        return NotNode(stBp)  # Return a new NotNode with standardized Bp
+
+    def __str__(self):
         return f"Not({self.Bp})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         self.Bp.print(indent + 1)
     
-class BAndOrNode(ASTNode):
+
+class BAndOrNode(Node):
     def __init__(self, B1, B2, operator):
         super().__init__('Boolean', operator)
         self.B1 = B1
         self.B2 = B2
     
     def standardize(self):
-        # Standardize logic goes here
+        stB1 = self.B1.standardize()
+        stB2 = self.B2.standardize()
+        return BAndOrNode(stB1, stB2, self.value)  # Return a new BAndOrNode with standardized B1 and B2
+    
+    def __str__(self):
         return f"{self.value}({self.B1}, {self.B2})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         self.B1.print(indent + 1)
         self.B2.print(indent + 1)
-    
-class ConditionNode(ASTNode):
+
+
+class ConditionNode(Node):
     def __init__(self, a1, a2, condition):
         super().__init__('condition', condition)
         self.a1 = a1
         self.a2 = a2
 
     def standardize(self):
-        # Standardize logic goes here
+        stA1 = self.a1.standardize()
+        stA2 = self.a2.standardize()
+        return ConditionNode(stA1, stA2, self.value)
+    
+    def __str__(self):
         return f"{self.value}({self.a1}, {self.a2})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         self.a1.print(indent + 1)
         self.a2.print(indent + 1)
-    
-class ArithmeticNode(ASTNode):
+
+
+class ArithmeticNode(Node):
     def __init__(self, operator, a1, a2):
         super().__init__('arithmetic', operator)
         self.a1 = a1
         self.a2 = a2
 
     def standardize(self):
-        # Standardize logic goes here
+        stA1 = self.a1.standardize()
+        stA2 = self.a2.standardize()
+        return ArithmeticNode(self.value, stA1, stA2)  # Return a new ArithmeticNode with standardized a1 and a2
+        
+    def __str__(self):
         return f"{self.value}({self.a1}, {self.a2})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         self.a1.print(indent + 1)
         self.a2.print(indent + 1)
-    
-class NegNode(ASTNode):
+
+
+class NegNode(Node):
     def __init__(self, a):
         super().__init__('neg', 'neg')
         self.a = a
     
     def standardize(self):
-        # Standardize logic goes here
+        stA = self.a.standardize()
+        return NegNode(stA)  # Return a new NegNode with standardized a
+    
+    def __str__(self):
         return f"Neg({self.a})"
     
     def print(self, indent=0):
         print(f'{self.indentationSymbol * indent}{self.value}')
         self.a.print(indent + 1)
-    
-class AtNode(ASTNode):
+
+
+class AtNode(Node):
     def __init__(self, a1, Id, a2):
         super().__init__('at', '@')
         self.a1 = a1
@@ -340,7 +472,12 @@ class AtNode(ASTNode):
         self.a2 = a2
     
     def standardize(self):
-        # Standardize logic goes here
+        stA1 = self.a1.standardize()
+        stA2 = self.a2.standardize()
+        tempGamma = GammaNode(self.Id, stA1)
+        return GammaNode(tempGamma, stA2)
+    
+    def __str__(self):
         return f"@({self.a1}, {self.Id}, {self.a2})"
     
     def print(self, indent=0):
